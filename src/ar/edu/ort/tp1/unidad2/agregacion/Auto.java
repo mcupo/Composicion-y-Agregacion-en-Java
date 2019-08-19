49bc24ca0805c412db0aaae6e5898846817dd7b5
Marco Cupo <mcupo@users.noreply.github.com>
2019-08-18 21:22:21 -0300

    Ejemplo de agregación

---
+++ src/ar/edu/ort/tp1/unidad2/agregacion/Auto.java
@@ -0,0 +1,45 @@
+package ar.edu.ort.tp1.unidad2.agregacion;
+
+import java.util.ArrayList;
+
+public class Auto {
+	
+	private String marca;
+	private String modelo;
+	//La clase Auto tiene una relación de agregación con la clase Pasajero
+	private ArrayList<Pasajero> pasajeros;
+	
+	/*
+	 * Como la relación es de agregación, el o los pasajeros no son requeridos para la creación
+	 * de un objeto Auto.
+	 */
+	public Auto(String marca, String modelo) {
+		this.pasajeros = new ArrayList<Pasajero>();
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+	
+	public void agregarPasajero(Pasajero pasajero){
+		this.pasajeros.add(pasajero);
+	}
+	
+	public void eliminarPasajero(Pasajero pasajero){
+		this.pasajeros.remove(pasajero);
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+	
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+}
---
+++ src/ar/edu/ort/tp1/unidad2/agregacion/Test.java
@@ -0,0 +1,30 @@
+package ar.edu.ort.tp1.unidad2.agregacion;
+
+public class Test {
+
+	public static void main(String[] args) {
+		/*
+		 * Como la relación es de agregación, puedo crear ambos objetos de manera separada,
+		 * ninguno depende del otro para su creación. Pero a diferencia de la asociación,
+		 * existe un dueño temporal, en este caso el Auto.
+		 */
+		Pasajero jose = new Pasajero("Jose", "Perez");
+		Auto fordFiesta = new Auto("Ford", "Fiesta");
+
+		/*
+		 * Jose se sube al Auto fordFiesta.
+		 * A partir de este momento, Jose tiene una agregación con el Auto.
+		 * Mientras dure esta relación, el Auto es dueño de Jose. Es una relación más fuerte que la asociación.
+		 * Si bien en este caso no sería posible, la agregación permite que muchos objetos compartan la misma 
+		 * relación. Por ejemplo una misma mascota puede tener varios Dueños.
+		 */
+		fordFiesta.agregarPasajero(jose);
+		
+		/*
+		 * Jose se baja del Auto.
+		 * A partir de este momento, Jose deja de tener una relación de agregación con el Auto.
+		 * El auto ya no es más "Dueño" de Jose.
+		 */
+		fordFiesta.eliminarPasajero(jose);
+	}
+}
